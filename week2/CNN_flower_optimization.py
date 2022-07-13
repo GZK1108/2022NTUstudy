8b07a0c5c9044d24b7905a28ed1bc0b095506596
GZK1108 <stanleywell1229@gmail.com>
2022-07-13 12:19:32 +0800

    第四次课堂练习

--- week2/CNN_flower_optimization.py
+++ week2/CNN_flower_optimization.py
@@ -23,12 +23,12 @@ test_root = "C:/Users/11453/PycharmProjects/riskassessment/data/Test/"
 # io.imshow(image)
 # plt.show() # show the picture
 
-# In current set, the score is 0.687
+# In current set, the accuracy is 0.9423
 batch_size = 32
 
 Generator = ImageDataGenerator()
-train_data = Generator.flow_from_directory(train_root, (100, 100), batch_size=batch_size)
-test_data = Generator.flow_from_directory(test_root, (100, 100), batch_size=batch_size)
+train_data = Generator.flow_from_directory(train_root, (150, 150), batch_size=batch_size)
+test_data = Generator.flow_from_directory(test_root, (150, 150), batch_size=batch_size)
 
 
 im = train_data[0][0][1]
@@ -41,7 +41,7 @@ num_classes = len([i for i in os.listdir(train_root)])
 
 model = Sequential()
 
-model.add(Conv2D(16, (3, 3), input_shape=(100, 100, 3), activation='relu')) # you can change (5,5) to (3,3)
+model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 model.add(Dropout(0.05))
 
@@ -59,7 +59,7 @@ model.add(Dropout(0.05))
 
 model.add(Flatten())
 
-model.add(Dense(256, activation='relu'))
+model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.05))
 
 model.add(Dense(64, activation='relu'))
@@ -68,7 +68,7 @@ model.add(Dropout(0.05))
 model.add(Dense(num_classes, activation="softmax"))
 model.summary()
 model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
-model.fit(train_data, batch_size = batch_size, epochs=1)
+model.fit(train_data, batch_size = batch_size, epochs=12)
 
 # score = model.evaluate(train_data)
 # print(score)
@@ -78,28 +78,3 @@ print(score)
 # save this model
 # from keras.models import save_model
 # save_model(model, "Flower")
-
-# Using Resnet
-from tensorflow import keras
-from keras.applications.resnet import ResNet50
-#from keras.applications.resnet50 import ResNet50
-from keras.models import Model
-import keras
-restnet = ResNet50(include_top=False, weights='imagenet', input_shape=(100,100,3))
-restnet.summary()
-
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, InputLayer
-from keras.models import Sequential
-from keras import optimizers
-model1 = Sequential()
-model1.add(restnet)
-model1.add(Flatten())
-model1.add(Dense(512, activation='relu'))
-model1.add(Dropout(0.3))
-model1.add(Dense(num_classes, activation='softmax'))
-model1.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
-model1.fit(train_data, batch_size = batch_size, epochs=1)
-
-score = model1.evaluate(test_data)
-print(score)
-
