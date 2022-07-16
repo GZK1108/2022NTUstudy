46f467d3d8400c2a074867483b9eebb2a4b3812d
GZK1108 <stanleywell1229@gmail.com>
2022-07-16 23:11:51 +0800

    使用resnet

--- week2/Alzheimeer CNN.py
+++ week2/Alzheimeer CNN.py
@@ -8,46 +8,31 @@ import os
 train_root = "C:/Users/11453/PycharmProjects/riskassessment/data/alzheimeer/train/"
 test_root = "C:/Users/11453/PycharmProjects/riskassessment/data/alzheimeer/test/"
 
-batch_size = 10
+batch_size = 32
 
 Generator = ImageDataGenerator()
 train_data = Generator.flow_from_directory(train_root, (150, 150), batch_size=batch_size)
 test_data = Generator.flow_from_directory(test_root, (150, 150), batch_size=batch_size)
 
 num_classes = len([i for i in os.listdir(train_root)])
-model = Sequential()
 
-model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-model.add(Dropout(0.05))
 
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-model.add(Dropout(0.05))
+from keras.applications.resnet import ResNet50
+import keras
+restnet = ResNet50(include_top=False, weights='imagenet', input_shape=(150,150,3))
+restnet.summary()
 
-model.add(Conv2D(64, (3, 3),activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-model.add(Dropout(0.2))
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, InputLayer
+from keras.models import Sequential
+from keras import optimizers
+model1 = Sequential()
+model1.add(restnet)
+model1.add(Flatten())
+model1.add(Dense(64, activation='relu'))
+model1.add(Dropout(0.05))
+model1.add(Dense(num_classes, activation='softmax'))
+model1.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+model1.fit(train_data, batch_size = batch_size, epochs=10)
 
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-model.add(Dropout(0.2))
-
-model.add(Flatten())
-
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.2))
-
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.2))
-
-model.add(Dense(num_classes, activation="softmax"))
-model.summary()
-
-model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
-model.fit(train_data, batch_size = batch_size, epochs=10)
-
-# score = model.evaluate(train_data)
-# print(score)
-score = model.evaluate(test_data)
+score = model1.evaluate(test_data)
 print(score)
